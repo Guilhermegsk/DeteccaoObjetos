1dcb97c2ddb4a7448cf7187604c120cb25c6a40e
guilhermegsk <taurusstar.tt@gmail.com>
2026-03-04 15:30:31 -0300

    fix: correção base64

--- app/main.py
+++ app/main.py
@@ -23,12 +23,20 @@ async def websocket_endpoint(websocket: WebSocket):
         try:
             data = await websocket.receive_text()
 
+            if "," in data:
+                data = data.split(",")[1]
+
             img_bytes = base64.b64decode(data)
             np_arr = np.frombuffer(img_bytes, np.uint8)
             frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-            print("Frame recebido:", frame.shape)
-            detections = detect(frame)
 
+            if frame is None:
+                print("Frame inválido ❌")
+                continue
+
+            print("Frame recebido:", frame.shape)
+
+            detections = detect(frame)
             await websocket.send_json(detections)
 
         except Exception as e:
